30a5d66191f4abe9280829aec85eb6000c672f1d
H-Mizusaki <43192033+H-Mizusaki@users.noreply.github.com>
2019-11-16 10:57:41 +0900

    軌道計算修正と計算結果グラフアップ
    SatPlaceCalcurate.jlで計算した、1分毎1000分間の衛星位置（地心直交座標系、緯度経度それぞれ）の結果をグラフとして作成

--- SatPlaceCalcurate.jl
+++ SatPlaceCalcurate.jl
@@ -5,10 +5,9 @@ gr()
 #=
 設定パラメータ
 =#
-DataNum =1  #シミュレータ反復回数
-dt = 60*5 ##シミュレータの計算間隔 [s]
-t = DatetoJD(2019, 11, 07, 16, 10, 00)   #シミュレート開始時刻＠ユリウス日
-
+DataNum =100 #シミュレータ反復回数
+dt = 60 ##シミュレータの計算間隔 [s]
+t = DatetoJD(2019, 11, 14, 07, 30, 00)   #シミュレート開始時刻＠ユリウス日
 
 #開始時のみ処理
 tles = read_tle("ISS_TLE.txt") #TLE読み込み
@@ -31,15 +30,18 @@ x_ecef_log = zeros(100, 3)    #衛星位置＠地心直交座標系
 x_geod_log = zeros(100, 3)    #衛星位置＠測地座標系
 v_ecef_log = zeros(100, 3)    #衛星速度＠地心直交座標系
 
+
 #シミュレーションループ
 for i=1:DataNum
     x_sat, v_sat = SatPlaceCal(i)
     x_ecef_log[i,:] = x_sat
     v_ecef_log[i,:] = v_sat
-    x_geo = ECEFtoGeodetic([x_sat[1]; x_sat[2]; x_sat[3]])
+    println(x_ecef_log[i,:])
+    x_geo = ECEFtoGeodetic([-1*x_sat[1]; x_sat[2]; x_sat[3]])
     x_geod_log[i,1]=x_geo[1]
     x_geod_log[i,2]=x_geo[2]
     x_geod_log[i,3]=x_geo[3]
+    println(x_geod_log[i,:])
 end
 
 
@@ -58,20 +60,24 @@ ylabel!("y")
 savefig("test2.png")
 =#
 
+
 #=
 #=***********************************************
 緯度経度に変換してGifでグラフを作成したい場合はコレ
 ***********************************************=#
-anim = @animate for i=1:1
+anim = @animate for i=1:DataNum
     x_sat, v_sat = SatPlaceCal(i)
     x_ecef_log[i,:] = x_sat
     v_ecef_log[i,:] = v_sat
+    #plot(-1 .* x_ecef_log[1:i,1], x_ecef_log[1:i, 2], x_ecef_log[1:i, 3])
+    
     x_geo = ECEFtoGeodetic([x_ecef_log[i,1]; x_ecef_log[i,2]; x_ecef_log[i,3]])
-    x_geod_log[i,1]=x_geo[1]
+    x_geod_log[i,1]=-1*x_geo[1]
     x_geod_log[i,2]=x_geo[2]
     x_geod_log[i,3]=x_geo[3]
-    plot(2*x_geod_log[1:i,2],x_geod_log[1:i,2])
+    plot(180 .+ rad2deg.(x_geod_log[1:i,2]),rad2deg.(x_geod_log[1:i,1]))
+    
 end
 
-gif(anim, "test4_anim.gif", fps = 10)
+gif(anim, "test5_anim.gif", fps = 50)
 =#
