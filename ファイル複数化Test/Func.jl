840b1e9fcd83215ffc3eb160a9858d6904a6021c
H-Mizusaki <43192033+H-Mizusaki@users.noreply.github.com>
2019-11-22 16:36:53 +0900

    ファイル複数化Test
    ファイルを分割した際にどこまで通用するのか確認した跡、とりあえず共有

---
+++ ファイル複数化Test/Func.jl
@@ -0,0 +1,41 @@
+
+#using Plots
+#gr()
+
+function plus(x,y)
+    z = x + y
+    return z
+end
+
+function minus(x,y)
+    z = x - y
+    return z
+end
+
+function Print()
+    for i=1:6
+        println(Data[i])
+    end
+end
+
+function OverWrite()
+    Num = 1.
+    println(Num)
+end
+
+function Pow(x,y)
+    z = 1
+    for i=1:y
+        z = mul(z,x)
+    end
+    return z
+end
+
+function Pl(u,v)
+    println(u)
+    println(v)
+    plot(u,v)
+    savefig("test3.png")
+    tles = read_tle("ISS_TLE.txt") #TLE読み込み
+    println(tles.ω)
+end
