0dc5b77385837f3d83dde58286e353bb86803cbe
NemotoTatsuaki <nemoto.t0208@gmail.com>
2019-11-22 20:21:40 +0900

    add atmospheric models which is the type of function.

--- atmospheric_models.jl
+++ atmospheric_models.jl
@@ -1,6 +1,6 @@
 using SatelliteToolbox
 
-#velosityvector norm[km/s]
+#noem of velocity vector v[km/s]
 v = 7.66#norm(V)
 
 #require height of spacecraft[km]
@@ -12,3 +12,9 @@ println(h)
 #require atmosphere density[kg/m^3]
 println(expatmosphere(h))
 
+#v:norm of velocity vector v 
+function atmospheric_models(v)
+    h = (398600 - 6378*v^2)/(v^2)
+    h = h*1e3
+    return expatmosphere(h)
+end
