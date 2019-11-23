138ce5d500390d9ce1f99b40241e3563fa6889bf
Takuya Tamura <entropy.apollo15@gmail.com>
2019-11-23 13:54:20 +0900

    座標変換/IGRFの追加

---
+++ Coordinate_Transform.jl
@@ -0,0 +1,23 @@
+using ReferenceFrameRotations
+using LinearAlgebra
+using Gnuplot
+
+# 位置と速度から座標変換のための方向余弦行列を求める
+function getDCM(v,r)
+    e1 = v / norm(v, 2)
+    e2 = - cross(r,v) / (norm(r,2) * norm(v,2))
+    e3 = - r/ norm(r,2)
+    C = inv_rotation(DCM([e1;e2;e3]))
+    return C
+end
+
+# 方向余弦行列を用いてベクトルを変換
+v = [2.0,0.0,0.0]
+r = [1.0,1.0,1.0]
+
+x = 1:10
+@gp x x.^2 "w l tit 'Parabola'"
+save("test2.gp")
+
+C = getDCM(v,r)
+println(C)
---
+++ test2.gp
@@ -0,0 +1,16 @@
+$data0 << EOD
+ 1 1
+ 2 4
+ 3 9
+ 4 16
+ 5 25
+ 6 36
+ 7 49
+ 8 64
+ 9 81
+ 10 100
+EOD
+
+plot  \
+  $data0 w l tit 'Parabola'
+
