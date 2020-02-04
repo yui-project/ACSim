d33aa7007372710d8ee01e8ba51de39e0974ff02
futuran <s1711161@s.tsukuba.ac.jp>
2020-02-04 14:28:11 +0900

    translate_coordinate

--- main.jl
+++ main.jl
@@ -4,6 +4,7 @@ include("external_model/external_model.jl")
 #include("dynamics/dynamics.jl")
 #include("satellite/satellite.jl")
 include("graph_plot.jl")
+include("coordinates.jl")
 
 using SatelliteToolbox
 using Dates
@@ -20,7 +21,12 @@ function main()
 
 	# 軌道計算については先に行い、全時間分を配列に保存する
 	JD_log, x_ecef_log, v_ecef_log, x_geod_log = orbit_cal(DataNum,dt,start_time,TLEFileName)
+
+	# 進行方向とSCOFx軸とのずれ（内積）
 	dotvs = zeros(DataNum)
+
+	# SCOF上での衛星の進行方向
+	direct_on_SCOFs = zeros(DataNum,3)
 
 	# 衛星外環境モデル用変数
 	mag_vecs = zeros(DataNum,3)
@@ -44,19 +50,21 @@ function main()
 		# 衛星外環境モデル計算
 		mag_vec, sun_vec, atoms_dens = external_model(current_time,x_ecef_log[i,:],x_geod_log[i,:])
 		println("         mag_vec:",mag_vec)
-		mag_vecs[i,:] = mag_vec
+		mag_vecs[i,:] = ecef_to_DCM(x_ecef_log[i,:],v_ecef_log[i,:],true)*mag_vec
 		println("         sun_vec:",sun_vec)
 		sun_vecs[i,:] = sun_vec
 		println("      atoms_dens:",atoms_dens)
 		atoms_denses[i] = atoms_dens
 
+		direct_on_SCOFs[i,:] = ecef_to_DCM(x_ecef_log[i,:],v_ecef_log[i,:],true) * v_ecef_log[i,:]
 
-		
+
 
 	end
 
 	plot_2scalar(JD_log,dotvs,"dotvs")
-	#=
+	plot_vec(direct_on_SCOFs,"direct_on_SCOFs")
+	
 	plot_2scalar(JD_log,atoms_denses,"atoms_dens")
 	plot_2scalar(JD_log,x_geod_log[:,3],"height")
 	plot_vec(mag_vecs,"mag_vec")
@@ -65,7 +73,7 @@ function main()
 	plot_vec(v_ecef_log,"v_ecef_log")
 	plot_vec(x_geod_log,"x_geod_log")
 	plot_2scalar(x_geod_log[:,2],x_geod_log[:,1],"x_geod_log_2d")
-	=#
+	
 
 
 	
