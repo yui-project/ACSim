670f47c45c8aeb9006546a38cd9c2ad8a9790a42
UGHyodo <s1811166@s.tsukuba.ac.jp>
2020-02-22 12:46:37 +0900

    update dynamics

--- dynamics/dynamics.jl
+++ dynamics/dynamics.jl
@@ -3,8 +3,8 @@ include("differencial.jl")
 include("RungeKutta.jl")
 
 function dynamics(q::Quaternion,ω::Vector,T::Vector,I::Matrix,dt)
-    dotq=RK4((q,ω),dt)
-    dotω=RK4((ω,T,I),dt) 
-    dotq/norm(dotq),dotω
+    qk=RK4((q,ω),dt)
+    ωk=RK4((ω,T,I),dt) 
+    qk/norm(qk),ωk
 end
 
