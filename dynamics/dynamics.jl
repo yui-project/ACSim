aec90959543f486fa66e2abaaa59b0be02de2a4d
UGHyodo <s1811166@s.tsukuba.ac.jp>
2020-02-22 10:19:24 +0900

    update dynamics

--- dynamics/dynamics.jl
+++ dynamics/dynamics.jl
@@ -1,4 +1,8 @@
 #(n)STEP目の姿勢から(n+1)STEP目の姿勢を計算する
-#状態変数
-#x=[q,ω]
-using SatelliteToolbox
+include("differencial.jl")
+include("RungeKutta.jl")
+
+function dynamics(q::Quaternion,ω::Vector,T::Vector,I::Matrix,dt)
+    RK4((q,ω),dt),RK4((ω,T,I),dt)
+end
+
