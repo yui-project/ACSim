3abf69a6d74fd1b1eb7552ef02c2bc109fc8bf19
UGHyodo <s1811166@s.tsukuba.ac.jp>
2020-02-29 04:16:02 +0900

    シミュレーションの問題を設定

--- dyntest.jl
+++ dyntest.jl
@@ -25,8 +25,18 @@ end
 
 l=10 #振り子の長さ
 m=10 #重りの重さ
+d=1 #重りの直径
+
 g=9.8*[0,0,-1] #重力加速度ベクトル
 r0=l*[1,0,0] #振り子の初期位置ベクトル
+#慣性テンソル
+Ix=0.4m*(d^2)
+Iy=0.4m*(d^2)+m*(l^2)
+Iz=0.4m*(d^2)+m*(l^2)
+I=
+[Ix 00 00
+;00 Iy 00
+;00 00 Iz]
 
 
 #トルクを求める関数
@@ -35,3 +45,18 @@ function torque(q::Quaternion,(r0,m,g)::Tuple{Vector,Number,Vector})
     T= cross(r,m*g)
     return T
 end
+
+#力学的エネルギーを求める関数
+function energy(q::Quaternion,ω::Vector,(r0,m,g)::Tuple{Vector,Number,Vector})
+    r=vect(q*r0*conj(q))
+    v=cross(r,ω)
+    f=m*g
+    E=0.5m*dot(v,v)-dot(r,f)+0.5
+    return E
+end
+
+#振り子のシミュレーション
+q=Quaternion(1,0,0,0)
+ω=[0,0,0]
+E=energy(q,ω,(r0,m,g))
+ans=[(q,ω,E)]
