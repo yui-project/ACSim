1cb28c615122b48bfb5fa564c89f39df44ce1e96
UGHyodo <s1811166@s.tsukuba.ac.jp>
2020-03-07 15:26:32 +0900

    引数を設定

--- satellite/attitude_determination.jl
+++ satellite/attitude_determination.jl
@@ -5,14 +5,13 @@ sat_pos, sat_att = attitude_determination()
 太陽センサと磁気センサから現在の位置・姿勢を決定
 
 # Argments
- - `sun_vecs`：センサから受け取った太陽方向ベクトル
- - `mag_vecs`：センサから受け取った地磁場方向ベクトル
- - `gyr_vecs`：センサから受け取った角速度ベクトル
-
+ - `(sun_vecs,mag_vecs,gyr_vecs)`：センサから受け取った太陽方向ベクトル,地磁場方向ベクトル,角速度ベクトルの組
+ - `(sun_ina,mag_ina)`：現在位置での感性座標系での太陽方向ベクトルと地磁場方向ベクトルの組
+ - `bef_quat`：1ステップ前の衛星の推定姿勢クォータニオン
 # Return
- - `sat_att`：衛星の推定姿勢クォータニオン
+ - `att_quat`：衛星の推定姿勢クォータニオン
 """
-function attitude_determination(sun_vecs::Vector,mag_vecs::Vector,gyr_vecs::Vector)
-    sat_att=Quaternion(1,0,0,0)
-    return sat_att
+function attitude_determination((sun_vecs,mag_vec,gyr_vecs),(sun_ina,mag_ina),bef_quat)
+    att_quat=Quaternion(1,0,0,0)
+    return att_quat
 end
