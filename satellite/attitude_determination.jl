918cb846abae9bf6d64fb336af94fc8c2a2f133a
UGHyodo <s1811166@s.tsukuba.ac.jp>
2020-03-11 11:36:41 +0900

    変数名を変更

--- satellite/attitude_determination.jl
+++ satellite/attitude_determination.jl
@@ -6,12 +6,12 @@ sat_pos, sat_att = attitude_determination()
 
 # Argments
  - `(sun_vecs,mag_vecs,gyr_vecs)`：センサから受け取った太陽方向ベクトル,地磁場方向ベクトル,角速度ベクトルの組
- - `(sun_ina,mag_ina)`：現在位置での感性座標系での太陽方向ベクトルと地磁場方向ベクトルの組
+ - `(isun_vecs,imag_vecs)`：現在位置での感性座標系での太陽方向ベクトルと地磁場方向ベクトルの組
  - `bef_quat`：1ステップ前の衛星の推定姿勢クォータニオン
 # Return
  - `att_quat`：衛星の推定姿勢クォータニオン
 """
-function attitude_determination((sun_vecs,mag_vec,gyr_vecs),(sun_ina,mag_ina),bef_quat)
+function attitude_determination((sun_vecs,mag_vec,gyr_vecs),(isun_vecs,imag_vecs),bef_quat)
     att_quat=Quaternion(1,0,0,0)
     return att_quat
 end
