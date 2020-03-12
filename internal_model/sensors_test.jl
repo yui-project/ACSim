de20f0746c4bf6ad08727fd836220caf225b9bb6
H-Mizusaki <43192033+H-Mizusaki@users.noreply.github.com>
2020-03-12 16:28:18 +0900

    fix - sensorsmodel & use it in internalmodel

--- internal_model/sensors_test.jl
+++ internal_model/sensors_test.jl
@@ -1,4 +1,14 @@
 include("sensors.jl")
+using SatelliteToolbox
+
+"""
+q1 = SatelliteToolbox.Quaternion(cos(π/4), 1.0*sin(π/4), 0.0*sin(π/4), 0.0*sin(π/4))
+q2 = SatelliteToolbox.Quaternion(cos(π/4), 0.0*sin(π/4), 0.0*sin(π/4), 1.0*sin(π/4))
+dt = 5
+
+ω = gyro_calcurate(q1, q2, dt)
+println(ω)
+"""
 
 """
 mag_vecs = [29.8522 -4.0303 35.8413]
@@ -8,6 +18,8 @@ println(mag_vols)
 
 """
 
-sun_vecs = [0.90 1.1111 0.383]
-sun_pos = sun_sensor(sun_vecs, "z-")
-println(sun_pos)
+sun_vecs = [-0.5 -0.5 1]
+ss_out = sun_sensor(sun_vecs, "z-")
+println(ss_out)
+
+
