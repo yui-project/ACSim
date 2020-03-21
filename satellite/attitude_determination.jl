b17b58ec733ad7718fc2d0f25aab927395767c97
UGHyodo <s1811166@s.tsukuba.ac.jp>
2020-03-21 15:00:58 +0900

    determinationを仮完成

--- satellite/attitude_determination.jl
+++ satellite/attitude_determination.jl
@@ -1,4 +1,5 @@
 using SatelliteToolbox
+using LinearAlgebra
 """
 sat_pos, sat_att = attitude_determination()
 
@@ -6,12 +7,14 @@ sat_pos, sat_att = attitude_determination()
 
 # Argments
  - `(sun_vecs,mag_vecs,gyr_vecs)`：センサから受け取った太陽方向ベクトル,地磁場方向ベクトル,角速度ベクトルの組
- - `(isun_vecs,imag_vecs)`：現在位置での感性座標系での太陽方向ベクトルと地磁場方向ベクトルの組
+ - `(isun_vecs,imag_vecs)`：現在位置での慣性座標系での太陽方向ベクトルと地磁場方向ベクトルの組
  - `bef_quat`：1ステップ前の衛星の推定姿勢クォータニオン
+ -`dt`：刻み幅
 # Return
  - `att_quat`：衛星の推定姿勢クォータニオン
 """
-function attitude_determination((sun_vecs,mag_vec,gyr_vecs),(isun_vecs,imag_vecs),bef_quat)
-    att_quat=Quaternion(1,0,0,0)
+function attitude_determination((sun_vecs,mag_vec,gyr_vecs),(isun_vecs,imag_vecs),bef_quat,dt)
+    att_quat=bef_quat+0.5bef_quat*gyr_vecs*dt
+    att_quat/=norm(att_quat)
     return att_quat
 end
